8ef97973801a38e6bad0d580d8e29b40c0f5b538
g610g <gio.gonzales@carsu.edu.ph>
2024-06-08 13:36:12 +0800

    multi producer single arc wrapped reciever attempt

--- src/lib.rs
+++ src/lib.rs
@@ -2,32 +2,48 @@ use std::collections::HashMap;
 use std::convert::Infallible;
 use std::sync::Arc;
 
+use crate::ws::Clients;
+use tokio::sync::mpsc;
+use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::sync::mpsc::UnboundedSender;
 use tokio::sync::Mutex;
+use warp::filters::ws::Message;
 use warp::Filter;
 use warp::Rejection;
 use warp::Reply;
-
-use crate::ws::Clients;
 pub mod ws;
 
 // pub const PORT: u32 = 8080;
 type Result<T> = std::result::Result<T, Rejection>;
-pub async fn ws_handler(ws: warp::ws::Ws, clients: Clients) -> Result<impl Reply> {
+type ChannelReciever<T> = Arc<UnboundedReceiver<T>>;
+pub async fn ws_handler(
+    ws: warp::ws::Ws,
+    clients: Clients,
+    _channel: (UnboundedSender<Message>, ChannelReciever<Message>),
+) -> Result<impl Reply> {
     //upgrade socket
     println!("Ws handler");
     Ok(ws.on_upgrade(move |socket| ws::client_connection(socket, clients.clone())))
 }
 pub async fn run() {
     let clients: Clients = Arc::new(Mutex::new(HashMap::new()));
+    let (rt, rx) = mpsc::unbounded_channel::<Message>();
     println!("Configuring websocket");
     let ws_route = warp::path("ws")
         .and(warp::ws())
         .and(with_clients(clients.clone()))
+        .and(with_channel((rt, Arc::new(rx))))
         .and_then(ws_handler);
     let routes = ws_route.with(warp::cors().allow_any_origin());
     warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
 }
-
+fn with_channel(
+    channel: (UnboundedSender<Message>, ChannelReciever<Message>),
+) -> impl Filter<Extract = ((UnboundedSender<Message>, ChannelReciever<Message>),), Error = Infallible>
+       + Clone {
+    let (rt, rx) = channel;
+    warp::any().map(move || (rt.clone(), rx.clone()))
+}
 fn with_clients(clients: Clients) -> impl Filter<Extract = (Clients,), Error = Infallible> + Clone {
     warp::any().map(move || clients.clone())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fmt::Debug;
 use std::ops::Add;
+
 #[derive(Debug)]
 struct Point {
     x: u32,
@@ -16,8 +17,5 @@ impl Add for Point {
 }
 #[tokio::main]
 async fn main() {
-    let point_a = Point { x: 1, y: 2 };
-    let point_b = Point { x: 3, y: 6 };
-    let new_point = point_a + point_b;
-    println!("{:?}", new_point);
+    docker_warp::run().await;
 }
