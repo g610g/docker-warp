20537fd63acfa8bf0afdbb71ca5b98e13f0e49d0
Gio Gonzales <gio.gonzales@carsu.edu.ph>
2024-06-14 22:13:06 +0800

    retrieves containers from the container interface

--- src/docker.rs
+++ src/docker.rs
@@ -1,22 +1,28 @@
-use docker_api::{self, opts::ContainerListOpts, Containers};
+use docker_api::{self, models::ContainerSummary, opts::ContainerListOpts, Container, Containers};
 pub async fn docker() {
     let docker = docker_api::Docker::unix("/var/run/docker.sock");
 
     let _ = docker.version().await.unwrap();
     let containers = docker.containers();
-    // println!("Containers: {:?}", containers);
-    list_containers(&containers).await;
+    let containers_list = give_me_container(&containers).await;
+    println!("Container List: {:?}", containers_list);
 }
-async fn list_containers(container: &Containers) {
+async fn give_me_container(container_interface: &Containers) -> Vec<Container> {
+    let mut result_containers = vec![];
+    let container_summaries = container_lists(&container_interface).await;
+    for container_summary in container_summaries {
+        match container_summary.id {
+            Some(id) => result_containers.push(container_interface.get(id)),
+            None => println!("No id for this container"),
+        };
+    }
+    return result_containers;
+}
+async fn container_lists(containers_interface: &Containers) -> Vec<ContainerSummary> {
     let builder = ContainerListOpts::builder();
     let container_list_opts = builder.all(true).build();
-    let container_list = container.list(&container_list_opts).await.unwrap();
-    for container in container_list {
-        match container.names {
-            Some(names) => {
-                println!("Name: {:?}", names);
-            }
-            None => {}
-        }
-    }
+    containers_interface
+        .list(&container_list_opts)
+        .await
+        .unwrap()
 }
